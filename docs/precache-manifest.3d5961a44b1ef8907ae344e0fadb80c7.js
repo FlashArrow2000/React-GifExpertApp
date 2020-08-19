42218c5ebd0af14a9872afd78f98e6c568a40d6c
Daniel Campos <danics2000@hotmail.com>
2020-08-19 13:53:15 -0600

    docs added

---
+++ docs/precache-manifest.3d5961a44b1ef8907ae344e0fadb80c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebd871c57dd480b80d01bdb80ae2150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cbeb01eb10342822818",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbeb01eb10342822818",
+    "url": "/static/js/main.520cde2a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
